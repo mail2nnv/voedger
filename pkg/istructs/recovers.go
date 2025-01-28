a17772c3b8701938bbf2997bfbc055fbd1e245fd
Nikolay <mail2nnv@gmail.com>
2025-01-28 11:02:56 +0300

    better istructs interface

--- pkg/istructs/recovers.go
+++ pkg/istructs/recovers.go
@@ -7,27 +7,42 @@ package istructs
 
 // Partition recovers
 type IRecovers interface {
-	Get(PartitionID, *PartitionRecoveryPoint) error
-	Put(PartitionID, PartitionRecoveryPoint) error
+	// Gets partition recovery point from storage
+	Get(PartitionID) (IPartitionRecoveryPoint, error)
+
+	// Puts partition recovery point to storage
+	Put(IPartitionRecoveryPoint) error
 }
 
 // Partition recovery point
-type PartitionRecoveryPoint struct {
+type IPartitionRecoveryPoint interface {
+	// Partition ID
+	PID() PartitionID
+
 	// Offset of the last committed event
-	PLogOffset Offset
+	PLogOffset() Offset
 
 	// Workspaces, which are handled by this partition
-	Workspaces map[WSID]WorkspaceRecoveryPoint
+	Workspaces() map[WSID]IWorkspaceRecoveryPoint
+
+	// Updates partition recovery point with new values.
+	// It is repeatedly called to update information about workspaces recovery points.
+	//
+	// If workspace recovery point with the same WSID not exists - it will be created, otherwise updated.
+	Update(plog Offset, ws WSID, wlog Offset, id, cid RecordID)
 }
 
-// WorkspaceRecoveryPoint
-type WorkspaceRecoveryPoint struct {
+// Workspace recovery point
+type IWorkspaceRecoveryPoint interface {
+	// Workspace ID
+	WSID() WSID
+
 	// Offset of the last committed event
-	WLogOffset Offset
+	WLogOffset() Offset
 
 	// Last base record ID
-	BaseRecordID RecordID
+	BaseRecordID() RecordID
 
 	// Last CDoc (CRecord) base record ID
-	CBaseRecordID RecordID
+	CBaseRecordID() RecordID
 }
--- pkg/istructsmem/internal/recovery/impl.go
+++ pkg/istructsmem/internal/recovery/impl.go
@@ -23,54 +23,160 @@ func NewRecovers(vr istructs.IViewRecords) *Recovers {
 	}
 }
 
-func (r Recovers) Get(pid istructs.PartitionID, point *istructs.PartitionRecoveryPoint) error {
-	kb := r.vr.KeyBuilder(prp_ViewName)
-	kb.PutInt64(prp_PID, int64(pid))
+func (r Recovers) Get(pid istructs.PartitionID) (istructs.IPartitionRecoveryPoint, error) {
+	point := NewPartitionRecoveryPoint(r.vr, pid)
 
-	return r.vr.Read(context.Background(), 0, kb, func(key istructs.IKey, value istructs.IValue) error {
-		switch ws := istructs.WSID(key.AsInt64(prp_WSID)); ws {
-		case 0:
-			point.PLogOffset = istructs.Offset(value.AsInt64(prp_Offset))
-		default:
-			point.Workspaces[ws] = istructs.WorkspaceRecoveryPoint{
-				WLogOffset:    istructs.Offset(value.AsInt64(prp_Offset)),
-				BaseRecordID:  value.AsRecordID(prp_BaseRecordID),
-				CBaseRecordID: value.AsRecordID(prp_CBaseRecordID),
-			}
-		}
-		return nil
-	})
+	if err := point.get(); err != nil {
+		return nil, err
+	}
+
+	return point, nil
 }
 
-func (r Recovers) Put(pid istructs.PartitionID, point istructs.PartitionRecoveryPoint) error {
+func (r Recovers) Put(point istructs.IPartitionRecoveryPoint) error {
+	return point.(*PartitionRecoveryPoint).put()
+}
 
-	wsKey := func(ws istructs.WSID) istructs.IKeyBuilder {
-		kb := r.vr.KeyBuilder(prp_ViewName)
-		kb.PutInt64(prp_PID, int64(pid))
-		kb.PutInt64(prp_WSID, int64(ws))
-		return kb
+// # Supports:
+//   - istructs.IPartitionRecoveryPoint
+type PartitionRecoveryPoint struct {
+	vr         istructs.IViewRecords
+	k          istructs.IKeyBuilder
+	v          istructs.IValueBuilder
+	pid        istructs.PartitionID
+	plog       istructs.Offset
+	workspaces map[istructs.WSID]istructs.IWorkspaceRecoveryPoint
+	modified   map[istructs.WSID]*WorkspaceRecoveryPoint
+}
+
+func NewPartitionRecoveryPoint(vr istructs.IViewRecords, pid istructs.PartitionID) *PartitionRecoveryPoint {
+	p := &PartitionRecoveryPoint{
+		vr:         vr,
+		k:          vr.KeyBuilder(prp_ViewName),
+		v:          vr.NewValueBuilder(prp_ViewName),
+		pid:        pid,
+		plog:       istructs.NullOffset,
+		workspaces: make(map[istructs.WSID]istructs.IWorkspaceRecoveryPoint),
+		modified:   make(map[istructs.WSID]*WorkspaceRecoveryPoint),
+	}
+	p.k.PutInt64(prp_PID, int64(pid))
+	return p
+}
+
+func (p PartitionRecoveryPoint) PID() istructs.PartitionID { return p.pid }
+
+func (p PartitionRecoveryPoint) PLogOffset() istructs.Offset { return p.plog }
+
+func (p PartitionRecoveryPoint) Workspaces() map[istructs.WSID]istructs.IWorkspaceRecoveryPoint {
+	return p.workspaces
+}
+
+func (p *PartitionRecoveryPoint) Update(plog istructs.Offset, wsID istructs.WSID, wlog istructs.Offset, id, cid istructs.RecordID) {
+	p.plog = plog
+
+	if wsID != 0 {
+		ws, ok := p.workspaces[wsID]
+		if !ok {
+			ws := NewWorkspaceRecoveryPoint(p.vr, p.PID(), wsID)
+			p.workspaces[wsID] = ws
+		}
+		wsp := ws.(*WorkspaceRecoveryPoint)
+		wsp.update(wlog, id, cid)
+		p.modified[wsID] = wsp
+	}
+}
+
+func (p *PartitionRecoveryPoint) get() (err error) {
+	if err = p.vr.Read(context.Background(), 0, p.key(), func(key istructs.IKey, value istructs.IValue) error {
+		switch ws := istructs.WSID(key.AsInt64(prp_WSID)); ws {
+		case istructs.NullWSID:
+			p.plog = istructs.Offset(value.AsInt64(prp_Offset))
+		default:
+			p.Update(
+				p.PLogOffset(),
+				ws,
+				istructs.Offset(value.AsInt64(prp_Offset)),
+				value.AsRecordID(prp_BaseRecordID),
+				value.AsRecordID(prp_CBaseRecordID),
+			)
+		}
+		return nil
+	}); err == nil {
+		clear(p.modified)
 	}
 
-	wsValue := func(ws istructs.WorkspaceRecoveryPoint) istructs.IValueBuilder {
-		vb := r.vr.NewValueBuilder(prp_ViewName)
-		vb.PutInt64(prp_Offset, int64(ws.WLogOffset))
-		vb.PutRecordID(prp_BaseRecordID, ws.BaseRecordID)
-		vb.PutRecordID(prp_CBaseRecordID, ws.CBaseRecordID)
-		return vb
-	}
+	return err
+}
 
-	batch := make([]istructs.ViewKV, 0, len(point.Workspaces)+1)
+func (p PartitionRecoveryPoint) key() istructs.IKeyBuilder { return p.k }
+
+func (p *PartitionRecoveryPoint) put() (err error) {
+	batch := make([]istructs.ViewKV, 0, len(p.modified)+1)
 	batch = append(batch, istructs.ViewKV{
-		Key:   wsKey(0),
-		Value: wsValue(istructs.WorkspaceRecoveryPoint{WLogOffset: point.PLogOffset}),
+		Key:   p.key(),
+		Value: p.value(),
 	})
 
-	for ws, wsPoint := range point.Workspaces {
+	for _, ws := range p.modified {
 		batch = append(batch, istructs.ViewKV{
-			Key:   wsKey(ws),
-			Value: wsValue(wsPoint),
+			Key:   ws.key(),
+			Value: ws.value(),
 		})
 	}
+	if err = p.vr.PutBatch(0, batch); err == nil {
+		clear(p.modified)
+	}
+	return err
+}
 
-	return r.vr.PutBatch(0, batch)
+func (p *PartitionRecoveryPoint) value() istructs.IValueBuilder {
+	p.v.PutInt64(prp_Offset, int64(p.plog))
+	return p.v
+}
+
+// # Supports:
+//   - istructs.IWorkspaceRecoveryPoint
+type WorkspaceRecoveryPoint struct {
+	vr   istructs.IViewRecords
+	k    istructs.IKeyBuilder
+	v    istructs.IValueBuilder
+	ws   istructs.WSID
+	wlog istructs.Offset
+	id   istructs.RecordID
+	cid  istructs.RecordID
+}
+
+func NewWorkspaceRecoveryPoint(vr istructs.IViewRecords, pid istructs.PartitionID, ws istructs.WSID) *WorkspaceRecoveryPoint {
+	w := &WorkspaceRecoveryPoint{
+		vr: vr,
+		k:  vr.KeyBuilder(prp_ViewName),
+		v:  vr.NewValueBuilder(prp_ViewName),
+		ws: ws,
+	}
+	w.k.PutInt64(prp_PID, int64(pid))
+	w.k.PutInt64(prp_WSID, int64(ws))
+	return w
+}
+
+func (w WorkspaceRecoveryPoint) WSID() istructs.WSID { return w.ws }
+
+func (w WorkspaceRecoveryPoint) WLogOffset() istructs.Offset { return w.wlog }
+
+func (w WorkspaceRecoveryPoint) BaseRecordID() istructs.RecordID { return w.id }
+
+func (w WorkspaceRecoveryPoint) CBaseRecordID() istructs.RecordID { return w.cid }
+
+func (w WorkspaceRecoveryPoint) key() istructs.IKeyBuilder { return w.k }
+
+func (w *WorkspaceRecoveryPoint) update(wlog istructs.Offset, id, cid istructs.RecordID) {
+	w.wlog = wlog
+	w.id = id
+	w.cid = cid
+}
+
+func (w *WorkspaceRecoveryPoint) value() istructs.IValueBuilder {
+	w.v.PutInt64(prp_Offset, int64(w.wlog))
+	w.v.PutRecordID(prp_BaseRecordID, w.id)
+	w.v.PutRecordID(prp_CBaseRecordID, w.cid)
+	return w.v
 }
